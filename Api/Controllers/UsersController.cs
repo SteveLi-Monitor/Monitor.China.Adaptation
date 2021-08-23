79219b0d40ae399a4a825422143d61501f8e65fa
SteveLi-Monitor <76800838+SteveLi-Monitor@users.noreply.github.com>
2021-08-23 20:07:29 +0800

    Add UsersController.SignIn method. (#8)

---
+++ Api/Controllers/UsersController.cs
@@ -0,0 +1,28 @@
+﻿using Application.Common.Jwt;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using System.Security.Claims;
+
+namespace Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        private readonly IConfiguration configuration;
+
+        public UsersController(IConfiguration configuration)
+        {
+            this.configuration = configuration;
+        }
+
+        [HttpPost("signin")]
+        public ActionResult<string> SignIn()
+        {
+            var option = configuration.GetSection(nameof(JwtTokenOption)).Get<JwtTokenOption>();
+            return new JwtTokenBuilder(option)
+                .AddClaim(new Claim("Username", "API01"))
+                .WriteToken();
+        }
+    }
+}
