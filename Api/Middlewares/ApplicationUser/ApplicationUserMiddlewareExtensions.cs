b47e2a8e1568c4a92f68b04651fb02d223815ed7
Steve Li <Steve.Li@monitorerp.com>
2021-08-24 16:34:41 +0800

    Add ApplicationUserMiddleware.

---
+++ Api/Middlewares/ApplicationUser/ApplicationUserMiddlewareExtensions.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Builder;
+
+namespace Api.Middlewares.ApplicationUser
+{
+    public static class ApplicationUserMiddlewareExtensions
+    {
+        public static IApplicationBuilder UseApplicationUser(this IApplicationBuilder builder)
+        {
+            return builder.UseMiddleware<ApplicationUserMiddleware>();
+        }
+    }
+}
