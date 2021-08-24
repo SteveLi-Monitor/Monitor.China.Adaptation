ee239e5d1341dc580b0e21f760b42352be400d8f
Steve Li <Steve.Li@monitorerp.com>
2021-08-24 13:41:58 +0800

    Add LoginController.

---
+++ Monitor.China.Api/Controllers/LoginController.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Monitor.China.Api.Bootstrap;
+using System;
+using System.Threading.Tasks;
+
+namespace Monitor.China.Api.Controllers
+{
+    [Route("api/Login")]
+    [ApiController]
+    public class LoginController : ControllerBase
+    {
+        private readonly ApiTransaction apiTransaction;
+
+        public LoginController(ApiTransaction apiTransaction)
+        {
+            this.apiTransaction = apiTransaction;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Login()
+        {
+            try
+            {
+                await apiTransaction.CreateAsync();
+                return Ok();
+            }
+            catch (Exception e)
+            {
+                return BadRequest($"{e.GetType().Name}: {e.Message}");
+            }
+        }
+
+        [HttpPost("WithCertificate")]
+        public async Task<IActionResult> LoginWithCertificate()
+        {
+            try
+            {
+                await apiTransaction.CreateWithCertificateAsync();
+                return Ok();
+            }
+            catch (Exception e)
+            {
+                return BadRequest($"{e.GetType().Name}: {e.Message}");
+            }
+        }
+
+    }
+}
