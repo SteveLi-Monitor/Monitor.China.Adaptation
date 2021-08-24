e0d15a1c7ef5bd0b0e31c8e3c0c172757b08a819
Steve Li <Steve.Li@monitorerp.com>
2021-08-24 15:45:07 +0800

    Update MonitorApiService and UsersController.

--- Api/Controllers/UsersController.cs
+++ Api/Controllers/UsersController.cs
@@ -1,7 +1,12 @@
-﻿using Application.Common.Jwt;
+﻿using Application.Common;
+using Application.Common.Jwt;
+using Application.Common.MonitorApi;
+using Domain.Common;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
+using Newtonsoft.Json;
 using System.Security.Claims;
+using System.Threading.Tasks;
 
 namespace Api.Controllers
 {
@@ -10,18 +15,34 @@ namespace Api.Controllers
     public class UsersController : ControllerBase
     {
         private readonly IConfiguration configuration;
+        private ApplicationUser applicationUser;
+        private readonly MonitorApiService monitorApiService;
 
-        public UsersController(IConfiguration configuration)
+        public UsersController(
+            IConfiguration configuration,
+            ApplicationUser applicationUser,
+            MonitorApiService monitorApiService)
         {
             this.configuration = configuration;
+            this.applicationUser = applicationUser;
+            this.monitorApiService = monitorApiService;
         }
 
         [HttpPost("signin")]
-        public ActionResult<string> SignIn()
+        public async Task<ActionResult<string>> SignInAsync(MonitorApiUser monitorApiUser)
         {
+            applicationUser.Username = monitorApiUser.Username;
+            applicationUser.Password = monitorApiUser.Password;
+            applicationUser.CompanyNumber = monitorApiUser.CompanyNumber;
+            applicationUser.LanguageCode = monitorApiUser.LanguageCode;
+
+            await monitorApiService.SignInAsync();
+
             var option = configuration.GetSection(nameof(JwtTokenOption)).Get<JwtTokenOption>();
             return new JwtTokenBuilder(option)
-                .AddClaim(new Claim("Username", "API01"))
+                .AddClaim(
+                    new Claim(nameof(ApplicationUser),
+                    JsonConvert.SerializeObject(applicationUser)))
                 .WriteToken();
         }
     }
--- Application/Common/MonitorApi/MonitorApiService.cs
+++ Application/Common/MonitorApi/MonitorApiService.cs
@@ -5,11 +5,16 @@ using Microsoft.Extensions.Configuration;
 using Newtonsoft.Json;
 using System;
 using System.Net.Http;
+using System.Threading.Tasks;
 
 namespace Application.Common.MonitorApi
 {
     public class MonitorApiService
     {
+        private readonly IConfiguration configuration;
+        private readonly ApplicationUser applicationUser;
+        private readonly MonitorServerSetting monitorServerSetting;
+
         public MonitorApiService(
             HttpClient httpClient,
             IConfiguration configuration,
@@ -20,27 +25,40 @@ namespace Application.Common.MonitorApi
             monitorApiServiceSetting.Guard(nameof(monitorApiServiceSetting));
             monitorApiServiceSetting.Guard();
 
-            var monitorServerSetting = configuration
+            monitorServerSetting = configuration
                 .GetSection(nameof(MonitorServerSetting)).Get<MonitorServerSetting>();
             monitorServerSetting.Guard(nameof(monitorServerSetting));
             monitorServerSetting.Guard();
 
+            httpClient.BaseAddress = new Uri(monitorApiServiceSetting.ServiceAddress);
+
+            HttpClient = httpClient;
+            this.configuration = configuration;
+            this.applicationUser = applicationUser;
+        }
+
+        public HttpClient HttpClient { get; }
+
+        public async Task SignInAsync()
+        {
+            SetMonitorApiSettingHeader();
+
+            using var response = await HttpClient.PostAsync("/api/Login", null);
+            response.EnsureSuccessStatusCode();
+        }
+
+        private void SetMonitorApiSettingHeader()
+        {
             applicationUser.Guard(nameof(applicationUser));
             applicationUser.Guard();
 
-            httpClient.BaseAddress = new Uri(monitorApiServiceSetting.ServiceAddress);
-
-            httpClient.DefaultRequestHeaders.Add(
+            HttpClient.DefaultRequestHeaders.Add(
                 Constants.MonitorApiSettingHeader,
                 JsonConvert.SerializeObject(new MonitorApiSetting
                 {
                     MonitorServerSetting = monitorServerSetting,
                     MonitorApiUser = applicationUser
                 }));
-
-            HttpClient = httpClient;
         }
-
-        public HttpClient HttpClient { get; }
     }
 }
