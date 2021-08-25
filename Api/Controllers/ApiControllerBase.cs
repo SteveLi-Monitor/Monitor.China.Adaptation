c75da73ec0a23ad7459dff24cda2e9671a525549
Steve Li <Steve.Li@monitorerp.com>
2021-08-25 12:01:53 +0800

    Setup MediatR. Add SignInCommand.

---
+++ Api/Controllers/ApiControllerBase.cs
@@ -0,0 +1,24 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Api.Controllers
+{
+    public abstract class ApiControllerBase : ControllerBase
+    {
+        private ISender sender;
+
+        protected ISender Mediator
+        {
+            get
+            {
+                if (sender == null)
+                {
+                    sender = HttpContext.RequestServices.GetRequiredService<ISender>();
+                }
+
+                return sender;
+            }
+        }
+    }
+}
--- Api/Controllers/UsersController.cs
+++ Api/Controllers/UsersController.cs
@@ -1,49 +1,17 @@
-﻿using Application.Common;
-using Application.Common.Jwt;
-using Application.Common.MonitorApi;
-using Domain.Common;
+﻿using Application.Users.Commands.SignIn;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Configuration;
-using Newtonsoft.Json;
-using System.Security.Claims;
 using System.Threading.Tasks;
 
 namespace Api.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
-    public class UsersController : ControllerBase
+    public class UsersController : ApiControllerBase
     {
-        private readonly IConfiguration configuration;
-        private ApplicationUser applicationUser;
-        private readonly MonitorApiService monitorApiService;
-
-        public UsersController(
-            IConfiguration configuration,
-            ApplicationUser applicationUser,
-            MonitorApiService monitorApiService)
-        {
-            this.configuration = configuration;
-            this.applicationUser = applicationUser;
-            this.monitorApiService = monitorApiService;
-        }
-
         [HttpPost("signin")]
-        public async Task<ActionResult<string>> SignInAsync(MonitorApiUser monitorApiUser)
+        public async Task<ActionResult<string>> SignInAsync(SignInCommand command)
         {
-            applicationUser.Username = monitorApiUser.Username;
-            applicationUser.Password = monitorApiUser.Password;
-            applicationUser.CompanyNumber = monitorApiUser.CompanyNumber;
-            applicationUser.LanguageCode = monitorApiUser.LanguageCode;
-
-            await monitorApiService.SignInAsync();
-
-            var option = configuration.GetSection(nameof(JwtTokenOption)).Get<JwtTokenOption>();
-            return new JwtTokenBuilder(option)
-                .AddClaim(
-                    new Claim(nameof(ApplicationUser),
-                    JsonConvert.SerializeObject(applicationUser)))
-                .WriteToken();
+            return Ok(await Mediator.Send(command));
         }
     }
 }
