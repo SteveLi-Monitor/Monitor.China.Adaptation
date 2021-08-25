838137421cf57baa88b98d1534704f415bbe7bc6
Steve Li <Steve.Li@monitorerp.com>
2021-08-25 16:18:00 +0800

    Setup AutoMapper.

---
+++ Application/Common/Mappings/MapFromBase.cs
@@ -0,0 +1,12 @@
+﻿using AutoMapper;
+
+namespace Application.Common.Mappings
+{
+    public abstract class MapFromBase<T> : IMapFrom<T>
+    {
+        public virtual void Mapping(Profile profile)
+        {
+            profile.CreateMap(typeof(T), GetType());
+        }
+    }
+}
