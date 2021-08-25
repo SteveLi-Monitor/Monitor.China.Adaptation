a9f5453e627d18f8d96fc3da77143dd2e99a4e5a
Steve Li <Steve.Li@monitorerp.com>
2021-08-25 15:27:22 +0800

    Add ExceptionLogBehaviour.

---
+++ Application/Common/Behaviours/ExceptionLogBehaviour.cs
@@ -0,0 +1,37 @@
+﻿using Application.Common.Exceptions;
+using MediatR;
+using Newtonsoft.Json;
+using Serilog;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Application.Common.Behaviours
+{
+    public class ExceptionLogBehaviour<TRequest, TResponse> : IPipelineBehavior<TRequest, TResponse>
+        where TRequest : IRequest<TResponse>
+    {
+        public async Task<TResponse> Handle(TRequest request, CancellationToken cancellationToken, RequestHandlerDelegate<TResponse> next)
+        {
+            try
+            {
+                return await next();
+            }
+            catch (ValidationException validationEx)
+            {
+                Log.Error($"Validation failed for request {typeof(TRequest).FullName}: {Environment.NewLine}" +
+                    $"Request: {JsonConvert.SerializeObject(request, Formatting.Indented)} {Environment.NewLine}" +
+                    $"Errors: {JsonConvert.SerializeObject(validationEx.Errors, Formatting.Indented)}");
+
+                throw;
+            }
+            catch (Exception e)
+            {
+                Log.Error(e, $"Unhandled exception for request {typeof(TRequest).FullName}: {Environment.NewLine}" +
+                    $"Request: {JsonConvert.SerializeObject(request, Formatting.Indented)}");
+
+                throw;
+            }
+        }
+    }
+}
