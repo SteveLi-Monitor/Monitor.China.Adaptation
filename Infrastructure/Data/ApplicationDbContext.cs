c4c1e1203cb8f3d00cef083a7a3609d600d55c49
Steve Li <Steve.Li@monitorerp.com>
2021-08-25 17:06:09 +0800

    Setup Infrastructure project with EntityFrameworkCore and MySql.

---
+++ Infrastructure/Data/ApplicationDbContext.cs
@@ -0,0 +1,25 @@
+﻿using Application.Common.Interfaces;
+using Microsoft.EntityFrameworkCore;
+using System.Reflection;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Data
+{
+    public class ApplicationDbContext : DbContext, IApplicationDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+
+        public Task<int> SaveChangesAsync()
+        {
+            return base.SaveChangesAsync();
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
