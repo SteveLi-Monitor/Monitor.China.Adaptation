d199eeca60562060125b5218da2a4dfffc80b987
SteveLi-Monitor <76800838+SteveLi-Monitor@users.noreply.github.com>
2021-08-25 17:58:18 +0800

    Add TestController for api projects. (#24)

---
+++ Monitor.China.Api/Controllers/TestController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Monitor.China.Api.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class TestController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<string> Get()
+        {
+            return Ok("Monitor.China.Api is running.");
+        }
+    }
+}
