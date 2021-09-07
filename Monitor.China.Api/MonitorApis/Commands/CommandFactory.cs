68fd1f8f4b615d66473c2518cce5cd56c9cf5aef
SteveLi-Monitor <76800838+SteveLi-Monitor@users.noreply.github.com>
2021-09-07 14:36:17 +0800

    Add SqlAnywhere connection. (#29)

--- Monitor.China.Api/MonitorApis/Commands/CommandFactory.cs
+++ Monitor.China.Api/MonitorApis/Commands/CommandFactory.cs
@@ -1,5 +1,6 @@
 ﻿using Monitor.API.Client.Repositories;
 using Monitor.China.Api.Bootstrap;
+using Monitor.China.Api.MonitorApis.Commands.Common;
 
 namespace Monitor.China.Api.MonitorApis.Commands
 {
@@ -17,6 +18,11 @@ namespace Monitor.China.Api.MonitorApis.Commands
         public GenericCommand Create()
         {
             return new GenericCommand(apiTransaction, monitorApiRepositoryFactory);
+        }
+
+        public ConfigurationCommand CreateConfigurationCommand()
+        {
+            return new ConfigurationCommand(apiTransaction, monitorApiRepositoryFactory);
         }
     }
 }
