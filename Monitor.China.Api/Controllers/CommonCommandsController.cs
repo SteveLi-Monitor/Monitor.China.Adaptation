d3b605c758aed8a4d632c8066d775882f3caf2fb
SteveLi-Monitor <76800838+SteveLi-Monitor@users.noreply.github.com>
2021-11-23 13:09:37 +0800

    Add MonitorApis.CommonCommands.GetMonitorConfiguration (#33)

---
+++ Monitor.China.Api/Controllers/CommonCommandsController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Monitor.API.Common.Commands.Configuration;
+using Monitor.China.Api.MonitorApis.Commands;
+using System.Threading.Tasks;
+
+namespace Monitor.China.Api.Controllers
+{
+    [Route("api/Common/Commands")]
+    [ApiController]
+    public class CommonCommandsController : ControllerBase
+    {
+        private readonly CommandFactory commandFactory;
+
+        public CommonCommandsController(CommandFactory commandFactory)
+        {
+            this.commandFactory = commandFactory;
+        }
+
+        [HttpPost("GetMonitorConfiguration")]
+        public async Task<ActionResult<MonitorConfigurationInfo>> GetMonitorConfiguration()
+        {
+            var configurationCommand = commandFactory.CreateConfigurationCommand();
+            return Ok(await configurationCommand.GetMonitorConfigurationAsync());
+        }
+    }
+}
