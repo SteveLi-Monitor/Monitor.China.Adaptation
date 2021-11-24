9380fb068d58d914a4d086d9b0426f5d89a5a711
Steve Li <Steve.Li@monitorerp.com>
2021-11-24 15:30:24 +0800

    Add Console project.

---
+++ Console/App.cs
@@ -0,0 +1,43 @@
+﻿using Serilog;
+using System;
+using System.Threading.Tasks;
+
+namespace Console
+{
+    internal class App
+    {
+        public async Task StartAsync()
+        {
+            Log.Debug($"{nameof(App)}.{nameof(StartAsync)}: Started.");
+
+            while (true)
+            {
+                System.Console.WriteLine(
+                    Environment.NewLine +
+                    "Menu: " + Environment.NewLine +
+                    "1. Import parts" + Environment.NewLine +
+                    "0. Exit" + Environment.NewLine);
+
+                var optionString = System.Console.ReadLine();
+
+                if (int.TryParse(optionString, out int option))
+                {
+                    switch (option)
+                    {
+                        case 0:
+                            Environment.Exit(0);
+                            break;
+
+                        default:
+                            Log.Error($"Invalid option '{option}'. Please enter a correct option.");
+                            break;
+                    }
+                }
+                else
+                {
+                    Log.Error($"Invalid option '{optionString}'. Please enter an integer.");
+                }
+            }
+        }
+    }
+}
---
+++ Console/Program.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Serilog;
+using System;
+using System.Threading.Tasks;
+
+namespace Console
+{
+    internal static class Program
+    {
+        private static async Task Main(string[] args)
+        {
+            var services = new ServiceCollection();
+
+            BootstrapApp(services);
+
+            using var serviceProvider = services.BuildServiceProvider();
+            var app = serviceProvider.GetRequiredService<App>();
+
+            await app.StartAsync();
+        }
+
+        private static void BootstrapApp(IServiceCollection services)
+        {
+            var configuration = BuildConfiguration();
+            BuildSerilog(configuration);
+            ConfigureServices(services);
+        }
+
+        private static IConfiguration BuildConfiguration()
+        {
+            return new ConfigurationBuilder()
+                .SetBasePath(AppDomain.CurrentDomain.BaseDirectory)
+                .AddJsonFile("appsettings.json", false)
+                .Build();
+        }
+
+        private static void BuildSerilog(IConfiguration configuration)
+        {
+            Log.Logger = new LoggerConfiguration()
+                .ReadFrom.Configuration(configuration)
+                .WriteTo.Console()
+                .WriteTo.File(
+                    AppDomain.CurrentDomain.BaseDirectory + "Logs/log.txt",
+                    rollingInterval: RollingInterval.Day)
+                .CreateLogger();
+        }
+
+        private static void ConfigureServices(IServiceCollection services)
+        {
+            services.AddSingleton<App>();
+        }
+    }
+}
