51563bb4dab560d456ba2cdf0c4c3154636e70ba
Steve Li <Steve.Li@monitorerp.com>
2021-12-09 17:55:51 +0800

    Remove Serilog when bootstrap Monitor.China.Api

--- Monitor.China.Api/Program.cs
+++ Monitor.China.Api/Program.cs
@@ -1,9 +1,7 @@
 ﻿using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
 using Serilog;
-using Serilog.Events;
 using System;
-using System.Reflection;
 
 namespace Monitor.China.Api
 {
@@ -11,28 +9,7 @@ namespace Monitor.China.Api
     {
         public static void Main(string[] args)
         {
-            Log.Logger = new LoggerConfiguration()
-                .MinimumLevel.Override("Microsoft", LogEventLevel.Error)
-                .Enrich.FromLogContext()
-                .WriteTo.Console()
-                .WriteTo.File(AppDomain.CurrentDomain.BaseDirectory + "Logs/start-up.txt")
-                .CreateLogger();
-
-            var assemblyName = Assembly.GetExecutingAssembly().GetName().Name;
-
-            try
-            {
-                Log.Information($"Starting web host: {assemblyName}.");
-                CreateWebHostBuilder(args).Build().Run();
-            }
-            catch (Exception e)
-            {
-                Log.Fatal(e, $"Web host terminated: {assemblyName}.");
-            }
-            finally
-            {
-                Log.CloseAndFlush();
-            }
+            CreateWebHostBuilder(args).Build().Run();
         }
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
