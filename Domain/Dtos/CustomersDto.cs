ca09a0789dd280dfc36a6bc61916bfb0e47176d2
Steve Li <Steve.Li@monitorerp.com>
2021-12-14 18:19:45 +0800

    Add CustomersController

---
+++ Domain/Dtos/CustomersDto.cs
@@ -0,0 +1,12 @@
+﻿namespace Domain.Dtos
+{
+    public class CustomersDto
+    {
+        public class Customer
+        {
+            public long Id { get; set; }
+
+            public string Code { get; set; }
+        }
+    }
+}
---
+++ Monitor.China.Api/Dtos/CustomerDto.cs
@@ -0,0 +1,11 @@
+﻿using Monitor.API.Client.Attributes;
+using Monitor.API.Infrastructure;
+
+namespace Monitor.China.Api.Dtos
+{
+    [ReadableEntity(ApiCategory.Sales, ApiEntityNamespace.Customer)]
+    public class CustomerDto : Domain.Dtos.CustomersDto.Customer
+    {
+        //Monitor.API.Sales.Customer
+    }
+}
