0de2b858bee074b0528d3282c8a865bfb0ec285a
Steve Li <Steve.Li@monitorerp.com>
2021-12-14 18:13:32 +0800

    Add CustomerOrdersController.

---
+++ Domain/Dtos/CustomerOrdersDto.cs
@@ -0,0 +1,12 @@
+﻿namespace Domain.Dtos
+{
+    public class CustomerOrdersDto
+    {
+        public class CustomerOrder
+        {
+            public long Id { get; set; }
+
+            public string OrderNumber { get; set; }
+        }
+    }
+}
---
+++ Monitor.China.Api/Dtos/CustomerOrderDto.cs
@@ -0,0 +1,11 @@
+﻿using Monitor.API.Client.Attributes;
+using Monitor.API.Infrastructure;
+
+namespace Monitor.China.Api.Dtos
+{
+    [ReadableEntity(ApiCategory.Sales, ApiEntityNamespace.CustomerOrder)]
+    public class CustomerOrderDto : Domain.Dtos.CustomerOrdersDto.CustomerOrder
+    {
+        //Monitor.API.Sales.CustomerOrder
+    }
+}
