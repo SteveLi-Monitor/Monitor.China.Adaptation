5d0eb6763f24de16ce0e2a5f31654ffae9307644
Steve Li <Steve.Li@monitorerp.com>
2021-12-14 10:21:51 +0800

    Add PartsController in Monitor.China.Api

---
+++ Monitor.China.Api/Controllers/PartsController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Monitor.API.Infrastructure;
+using Monitor.API.Inventory.Commands.Parts;
+using Monitor.China.Api.Dtos;
+using Monitor.China.Api.MonitorApis.Commands;
+using Monitor.China.Api.MonitorApis.Queries;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Monitor.China.Api.Controllers
+{
+    [Route("api/Inventory/Parts")]
+    [ApiController]
+    public class PartsController : ControllerBase
+    {
+        private readonly GenericQuery<PartDto> query;
+        private readonly GenericCommand command;
+
+        public PartsController(QueryFactory queryFactory, CommandFactory commandFactory)
+        {
+            query = queryFactory.Create<PartDto>();
+            command = commandFactory.Create();
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<PartDto>>> Get(string options)
+        {
+            return Ok(await query.GetAsync(options));
+        }
+
+        [HttpPost("Create")]
+        public async Task<ActionResult<EntityCommandResponse>> Create(CreatePart createPart)
+        {
+            return Ok(await command.SendAsync(createPart));
+        }
+    }
+}
---
+++ Monitor.China.Api/Dtos/PartDto.cs
@@ -0,0 +1,11 @@
+﻿using Monitor.API.Client.Attributes;
+using Monitor.API.Infrastructure;
+
+namespace Monitor.China.Api.Dtos
+{
+    [ReadableEntity(ApiCategory.Inventory, ApiEntityNamespace.Part)]
+    public class PartDto : Domain.Dtos.PartsDto.Part
+    {
+        //Monitor.API.Inventory.Part
+    }
+}
