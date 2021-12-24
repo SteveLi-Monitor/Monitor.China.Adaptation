4433dfe59811c6018760ee8f58fbd22333567b71
Steve Li <Steve.Li@monitorerp.com>
2021-12-24 10:57:49 +0800

    Add UserRoles.Queries.GetAll

---
+++ Api/Controllers/UserRolesController.cs
@@ -0,0 +1,19 @@
+﻿using Application.UserRoles.Queries.GetAll;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [Authorize]
+    public class UserRolesController : ApiControllerBase
+    {
+        [HttpGet]
+        public async Task<ActionResult<GetAllQueryResp>> Get()
+        {
+            return Ok(await Mediator.Send(new GetAllQuery()));
+        }
+    }
+}
---
+++ Application/UserRoles/Queries/GetAll/GetAllQueryResp.cs
@@ -0,0 +1,20 @@
+﻿using Application.Common.Mappings;
+using Application.Entities;
+using System.Collections.Generic;
+
+namespace Application.UserRoles.Queries.GetAll
+{
+    public class GetAllQueryResp
+    {
+        public IEnumerable<UserRole> UserRoles { get; set; }
+
+        public class UserRole : MapFromBase<Entities.UserRole>
+        {
+            public int Id { get; set; }
+
+            public string Name { get; set; }
+
+            public IList<UiComponent> AllowedUiComponents { get; set; }
+        }
+    }
+}
