c7d7009dfce65e1e49d924f518ceaf8619dd5913
SteveLi-Monitor <76800838+SteveLi-Monitor@users.noreply.github.com>
2021-12-24 13:30:38 +0800

    Add UserRoles.Commands.Update, UserRoles.Commands.Delete (#51)
    * Add UserRoles.Commands.Update
    
    * Add UserRoles.Commands.Delete

---
+++ Application/Common/ErrorMessages/EntityNotFound.cs
@@ -0,0 +1,29 @@
+﻿namespace Application.Common.ErrorMessages
+{
+    public class EntityNotFound : IErrorMessage
+    {
+        public EntityNotFound(string entityName, string propertyName, object propertyValue)
+        {
+            EntityName = entityName;
+            PropertyName = propertyName;
+            PropertyValue = propertyValue;
+        }
+
+        public EntityNotFound(string entityName, object propertyValue)
+            : this(entityName, "Id", propertyValue)
+        {
+        }
+
+        public string EntityName { get; private set; }
+
+        public string PropertyName { get; private set; }
+
+        public object PropertyValue { get; private set; }
+
+        public string ToMessage()
+        {
+            return $"{EntityName} not found. " +
+                $"Property name: {PropertyName}. Property value: {PropertyValue}.";
+        }
+    }
+}
---
+++ Application/UserRoles/Commands/Delete/DeleteCommand.cs
@@ -0,0 +1,34 @@
+﻿using Application.Common.Interfaces;
+using MediatR;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Application.UserRoles.Commands.Delete
+{
+    public class DeleteCommand : IValidateRequest<Unit>
+    {
+        public int Id { get; set; }
+
+
+        public bool NeedValidation { get; set; } = true;
+    }
+
+    public class DeleteCommandHandler : IRequestHandler<DeleteCommand>
+    {
+        private readonly IApplicationDbContext dbContext;
+
+        public DeleteCommandHandler(IApplicationDbContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+
+        public async Task<Unit> Handle(DeleteCommand request, CancellationToken cancellationToken)
+        {
+            var userRole = await dbContext.UserRoles.FindAsync(request.Id);
+            dbContext.UserRoles.Remove(userRole);
+
+            await dbContext.SaveChangesAsync();
+            return Unit.Value;
+        }
+    }
+}
