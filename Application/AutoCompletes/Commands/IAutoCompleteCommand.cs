8ffdf23706456bc1bd1733fbe61e29e26ca8890d
Steve Li <Steve.Li@monitorerp.com>
2021-12-24 12:11:18 +0800

    Remove ValidateRequestBase

---
+++ Application/AutoCompletes/Commands/IAutoCompleteCommand.cs
@@ -0,0 +1,11 @@
+﻿using Application.Common.Interfaces;
+
+namespace Application.AutoCompletes.Commands
+{
+    public interface IAutoCompleteCommand<TResponse> : IValidateRequest<TResponse>
+    {
+        int PageSize { get; set; }
+
+        string Filter { get; set; }
+    }
+}
