97e5256c0685f305372b1e3a855a13a66ae536b6
SteveLi-Monitor <steve.li@monitorerp.com>
2022-01-04 17:24:28 +0800

    Temp update

--- Infrastructure/Configurations/ApplicationUserEtc.cs
+++ Infrastructure/Configurations/ApplicationUserEtc.cs
@@ -9,6 +9,11 @@ namespace Infrastructure.Configurations
     {
         public void Configure(EntityTypeBuilder<ApplicationUser> builder)
         {
+            builder.Ignore(x => x.ApiUsername);
+            builder.Ignore(x => x.Password);
+            builder.Ignore(x => x.LanguageCode);
+            builder.Ignore(x => x.CompanyNumber);
+
             builder.Property(x => x.Username).IsRequired();
             builder.Property(x => x.UiComponents).HasJsonConversion();
 
